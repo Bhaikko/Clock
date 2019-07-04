4a3bab76f88e4ed041b6e9bb38c5d541445b0760
Bhaikko <siddhartha.pawar17@gmail.com>
2019-07-04 10:47:59 +0530

    Selectivity Added for Timer

--- Javascripts/LeftbarHandler.js
+++ Javascripts/LeftbarHandler.js
@@ -24,11 +24,14 @@ function enableClock()
     clock.classList.add("active");
     stopwatch.classList.remove("active");
     splitbox.classList.remove("active");
+    timer.classList.remove("active");
     enableLeftBar();
     clockButton[0].parentElement.classList.add("selected");
     clockButton[1].parentElement.classList.add("selected");
     stopwatchButton[0].parentElement.classList.remove("selected");
     stopwatchButton[1].parentElement.classList.remove("selected");
+    timerButton[0].parentElement.classList.remove("selected");
+    timerButton[1].parentElement.classList.remove("selected");
 }
 
 function enableStopwatch()
@@ -36,16 +39,36 @@ function enableStopwatch()
     clock.classList.remove("active");
     stopwatch.classList.add("active");
     splitbox.classList.add("active");
+    timer.classList.remove("active");
     enableLeftBar();
 
     clockButton[0].parentElement.classList.remove("selected");
     clockButton[1].parentElement.classList.remove("selected");
     stopwatchButton[0].parentElement.classList.add("selected");
     stopwatchButton[1].parentElement.classList.add("selected");
+    timerButton[0].parentElement.classList.remove("selected");
+    timerButton[1].parentElement.classList.remove("selected");
+}
+
+function enableTimer()
+{
+    clock.classList.remove("active");
+    stopwatch.classList.remove("active");
+    splitbox.classList.remove("active");
+    timer.classList.add("active");
+    enableLeftBar();
+
+    clockButton[0].parentElement.classList.remove("selected");
+    clockButton[1].parentElement.classList.remove("selected");
+    stopwatchButton[0].parentElement.classList.remove("selected");
+    stopwatchButton[1].parentElement.classList.remove("selected");
+    timerButton[0].parentElement.classList.add("selected");
+    timerButton[1].parentElement.classList.add("selected");
 }
 
 clockButton[0].addEventListener("click", enableClock);
 clockButton[1].addEventListener("click", enableClock);
 stopwatchButton[0].addEventListener("click", enableStopwatch);
 stopwatchButton[1].addEventListener("click", enableStopwatch);
-timerButton[1].addEventListener("click", enableLeftBar);
+timerButton[0].addEventListener("click", enableTimer);
+timerButton[1].addEventListener("click", enableTimer);
