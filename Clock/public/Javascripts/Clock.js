d82744bdcab6aa4e6187484f4745c9125adeff59
Bhaikko <siddhartha.pawar17@gmail.com>
2019-08-04 23:12:47 +0530

    World Clock Added

--- Clock/public/Javascripts/Clock.js
+++ Clock/public/Javascripts/Clock.js
@@ -8,51 +8,109 @@ let dateClock = $("#dateClock");
 let monthClock = $("#monthClock");
 let yearClock = $("#yearClock");
 
+let zonesList = $("#zonesList");
+let fetchedDate = null;
+
+let currentDay = null;
+let currentTime = null;
+let currentHour = null;
+let currentMinute = null;
+let currentSecond = null;
+let currentDate = null;
+let currentMonth= null;
+let currentYear = null;
+
+
+$.get("/getZones", (zones) =>
+{
+    zones.map((zone) =>
+    {
+        zonesList.append(`<div class="dropdown-item">${zone}</div>`);
+    });
+
+    zonesList.children().map((index) =>
+    {
+        zonesList.children()[index].addEventListener("click", (event) =>
+        {
+            let cityAndCountry = event.target.innerText.split("/");
+            let city = cityAndCountry[1];
+            let country = cityAndCountry[0];
+
+            $("#place").text(city + ", " + country);
+            jQuery.post("/getTime", {zone: zonesList.children()[index].innerText}, (data) => 
+            {
+                fetchedDate = data;
+                let currentData = data.split(" ");
+
+                currentDay = currentData[0];
+                currentTime = currentData[4].split(":");
+                currentHour = currentTime[0];
+                currentMinute = currentTime[1];
+                currentSecond = currentTime[2];
+                currentDate = currentData[2];
+                currentMonth = currentData[1];
+                currentYear = currentData[3];
+            });
+        });
+    });
+});
 
 function setClockData()
 {
-    // bClockRunning = clock[0].getAttribute("class").includes("active");
-    if(bClockRunning)
-    {
-        // console.log("Clock Running");
-        const currentDate = (new Date()).toString().split(" ");
-    
+    if(bClockRunning && fetchedDate)
+    {    
         //Day Setup
-        if(currentDate[0]=="Sun")
+        if(currentDay == "Sun")
             day.text("Sunday");
-        else if(currentDate[0]=="Mon")
+        else if(currentDay == "Mon")
             day.text("Monday");
-        else if(currentDate[0]=="Tue")
+        else if(currentDay == "Tue")
             day.text("Tuesday");
-        else if(currentDate[0]=="Wed")
+        else if(currentDay == "Wed")
             day.text("Wednesday");
-        else if(currentDate[0]=="Thu")
+        else if(currentDay == "Thu")
             day.text("Thursday");
-        else if(currentDate[0]=="Fri")
+        else if(currentDay == "Fri")
             day.text("Friday");
-        else if(currentDate[0]=="Sat")
+        else if(currentDay == "Sat")
             day.text("Saturday");
 
         //Time Of Day Setup
-        const time = currentDate[4].split(":");
-        if(time[0]>="06" && time[0]<"12")
+        if(currentHour>="06" && currentHour<"12")
             timeOfDay.text("Morning");
-        else if(time[0]>="12" && time[0]<"17")
+        else if(currentHour>="12" && currentHour<"17")
             timeOfDay.text("Afternoon");
-        else if(time[0]>="17" && time[0]<"20")
+        else if(currentHour>="17" && currentHour<"20")
             timeOfDay.text("Evening");
         else
             timeOfDay.text("Night");
 
         //Time Setup
-        hourClock.text(time[0] + " : ");
-        minuteClock.text(time[1] + " : ");
-        secondClock.text(time[2]);
+        hourClock.text(currentHour + " : ");
+        minuteClock.text(currentMinute + " : ");
+        secondClock.text(currentSecond);
 
         //Date Setup
-        dateClock.text(currentDate[2] + " - ");
-        monthClock.text(currentDate[1] + " - ");
-        yearClock.text(currentDate[3]);
+        dateClock.text(currentDate + " - ");
+        monthClock.text(currentMonth + " - ");
+        yearClock.text(currentYear);
+
+        currentSecond++;
+        if(currentSecond >= 59)
+        {
+            currentMinute++;
+            currentSecond = 0;
+            if(currentMinute >= 59)
+            {
+                currentMinute = 0;
+                currentHour++;
+                if(currentHour >= 24)
+                    currentHour = 0;
+
+            }
+        }
+
+
     }
 }
 
--- Clock/server.js
+++ Clock/server.js
@@ -1,8 +1,29 @@
 const express = require("express");
-
+const moment = require("moment-timezone");
 const server = express();
 
+// console.log(moment().tz("America/Los_Angeles").toString());
+// console.log(moment().tz("Indonesia/Amdon").toString());
+
 server.use(express.static("./public"));
+server.use(express.json());
+server.use(express.urlencoded({extended:    true}));
+
+server.get("/getZones", function(req, res)
+{
+    let zones = moment.tz.names();
+    zones = zones.filter((zone) =>
+    {
+        if(!zone.includes("Etc/"))
+            return zone;
+    })
+    res.send(zones);
+});
+
+server.post("/getTime", (req, res) =>
+{
+    res.send(moment().tz(req.body.zone).toString());
+})
 
 const port = 4000;
 server.listen(port, () => console.log("Server Up And Running on 127.0.0.1:" + port));
